a0393df428eaccc54aa974f1b1d8fc96e9445587
sun-yryr <taittide@gmail.com>
2019-08-01 00:00:51 +0900

    Initial Commit

---
+++ Swift-lib-atcoder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-lib-atcoder
+//
+//  Created by 皆川泰陽 on 2019/08/01.
+//  Copyright © 2019 皆川泰陽. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
